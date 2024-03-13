658ef0c918d4b3053a60ec51eb01ecb483a2d32a
Liliane Top <hallo@lilianetop.nl>
2024-03-13 11:07:45 +0100

    initial commit

---
+++ src/main/java/nl/top/openauthorizationserver/config/SecurityConfig.java
@@ -0,0 +1,4 @@
+package nl.top.openauthorizationserver.config;
+
+public class SecurityConfig {
+}
---
+++ src/test/java/nl/top/openauthorizationserver/OpenAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.top.openauthorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenAuthorizationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
