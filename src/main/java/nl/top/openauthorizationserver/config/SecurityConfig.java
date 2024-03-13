a00e102bd19bcb8093ae92d6233a757c831ca8c3
Liliane Top <hallo@lilianetop.nl>
2024-03-13 11:33:07 +0100

    adding UserDetailsService as part of default SpringSecurityConfig which will not be used in the Oauth flow we're building

--- src/main/java/nl/top/openauthorizationserver/config/SecurityConfig.java
+++ src/main/java/nl/top/openauthorizationserver/config/SecurityConfig.java
@@ -6,8 +6,12 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.MediaType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
@@ -53,6 +57,19 @@ public class SecurityConfig {
                 .formLogin(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    //this functionality will no tbe used as this will only be called in combination with
+    // form login part of the defaultSecurityFilterChain
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
     }
 
 
